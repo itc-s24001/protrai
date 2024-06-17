d9eb7b35f64e591a18d7c8aa2d01617f652bd955
itc-s24001 <itc-s24001@std.it-college.ac.jp>
2024-06-17 15:42:04 +0900

    add def4_kadai.py

--- def4_kadai.py
+++ def4_kadai.py
@@ -1,10 +1,14 @@
 #s24001
-#関数でおみくじをする
+#main()関数でおみくじをする
 
 import random
-def main():
+def omikuji():
     kuji = ["大吉","中吉","小吉","凶"]
     return random.choice(kuji)
 
-kekka = main()
-print("結果は", kekka, "です")
+def main():
+    kekka = omikuji()
+    print("結果は", kekka, "です")
+
+if __name__ == "__main__":
+    main()
